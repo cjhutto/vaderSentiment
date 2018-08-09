4e71cffec1128eeda387fc36edcde50d4c99c354
Jake Biesinger <jake.biesinger@gmail.com>
2018-08-09 16:05:59 -0700

    Update vaderSentiment.py

--- vaderSentiment/vaderSentiment.py
+++ vaderSentiment/vaderSentiment.py
@@ -208,7 +208,7 @@ class SentimentIntensityAnalyzer(object):
     """
 
     def __init__(self, lexicon_file="vader_lexicon.txt", emoji_lexicon="emoji_utf8_lexicon.txt"):
-        _this_module_file_path_ = os.path.abspath(__file__)
+        _this_module_file_path_ = __file__
         lexicon_full_filepath = os.path.join(os.path.dirname(_this_module_file_path_), lexicon_file)
         with open(lexicon_full_filepath, encoding='utf-8') as f:
             self.lexicon_full_filepath = f.read()
