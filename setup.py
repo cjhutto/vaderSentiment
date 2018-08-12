e07845486bbdbd191253ee599013b7d802207768
Seonha Park <camelia0858@gmail.com>
2018-08-12 23:52:51 +0900

    add dependency (requests)

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
   #packages = ['vaderSentiment'], # this must be the same as the name above
   packages = find_packages(exclude=['tests*']), # a better way to do it than the line above -- this way no typo/transpo errors
   include_package_data=True,
+  install_requires=['requests'],
   version = '3.2.1',
   description = 'VADER Sentiment Analysis. VADER (Valence Aware Dictionary and sEntiment Reasoner) is a lexicon and rule-based sentiment analysis tool that is specifically attuned to sentiments expressed in social media, and works well on texts from other domains.',
   long_description = read("README.rst"),
