b5655aa114be445597009a4ff4570dad1f8bae1a
youhealthy <tl1848@nyu.edu>
2018-11-26 10:59:40 -0500

    required dependecies

--- setup.py
+++ setup.py
@@ -15,6 +15,9 @@ setup(
   name = 'vaderSentiment',
   #packages = ['vaderSentiment'], # this must be the same as the name above
   packages = find_packages(exclude=['tests*']), # a better way to do it than the line above -- this way no typo/transpo errors
+  install_requires = [
+    "requests",
+  ],
   include_package_data=True,
   version = '3.2.1',
   description = 'VADER Sentiment Analysis. VADER (Valence Aware Dictionary and sEntiment Reasoner) is a lexicon and rule-based sentiment analysis tool that is specifically attuned to sentiments expressed in social media, and works well on texts from other domains.',
@@ -24,8 +27,8 @@ setup(
   author_email = 'cjhutto@gatech.edu',
   license = 'MIT License: http://opensource.org/licenses/MIT',
   url = 'https://github.com/cjhutto/vaderSentiment', # use the URL to the github repo
-  download_url = 'https://github.com/cjhutto/vaderSentiment/archive/master.zip', 
-  keywords = ['vader', 'sentiment', 'analysis', 'opinion', 'mining', 'nlp', 'text', 'data', 
+  download_url = 'https://github.com/cjhutto/vaderSentiment/archive/master.zip',
+  keywords = ['vader', 'sentiment', 'analysis', 'opinion', 'mining', 'nlp', 'text', 'data',
               'text analysis', 'opinion analysis', 'sentiment analysis', 'text mining', 'twitter sentiment',
               'opinion mining', 'social media', 'twitter', 'social', 'media'], # arbitrary keywords
   classifiers = ['Development Status :: 4 - Beta', 'Intended Audience :: Science/Research',
