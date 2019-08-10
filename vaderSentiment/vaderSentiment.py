b045da3c5a29ed130777a16dc78588f53da54a05
C.J. Hutto <cjhutto@users.noreply.github.com>
2019-08-10 12:11:53 -0400

    Update vaderSentiment.py
    Addresses issue #84 - included adjective forms of adverbs in booster dictionary (for those not already accounted for with an empirically derived valence in the lexicon).

--- vaderSentiment/vaderSentiment.py
+++ vaderSentiment/vaderSentiment.py
@@ -45,22 +45,27 @@ NEGATE = \
 # http://en.wiktionary.org/wiki/Category:English_degree_adverbs
 
 BOOSTER_DICT = \
-    {"absolutely": B_INCR, "amazingly": B_INCR, "awfully": B_INCR, "completely": B_INCR, "considerably": B_INCR,
-     "decidedly": B_INCR, "deeply": B_INCR, "effing": B_INCR, "enormously": B_INCR,
-     "entirely": B_INCR, "especially": B_INCR, "exceptionally": B_INCR, "extremely": B_INCR,
-     "fabulously": B_INCR, "flipping": B_INCR, "flippin": B_INCR,
-     "fricking": B_INCR, "frickin": B_INCR, "frigging": B_INCR, "friggin": B_INCR, "fully": B_INCR, "fucking": B_INCR,
-     "greatly": B_INCR, "hella": B_INCR, "highly": B_INCR, "hugely": B_INCR, "incredibly": B_INCR,
-     "intensely": B_INCR, "majorly": B_INCR, "more": B_INCR, "most": B_INCR, "particularly": B_INCR,
+    {"absolutely": B_INCR, "amazingly": B_INCR, "awfully": B_INCR, 
+     "completely": B_INCR, "considerable": B_INCR, "considerably": B_INCR,
+     "decidedly": B_INCR, "deeply": B_INCR, "effing": B_INCR, "enormous": B_INCR, "enormously": B_INCR,
+     "entirely": B_INCR, "especially": B_INCR, "exceptional": B_INCR, "exceptionally": B_INCR, 
+     "extreme": B_INCR, "extremely": B_INCR,
+     "fabulously": B_INCR, "flipping": B_INCR, "flippin": B_INCR, "frackin": B_INCR, "fracking": B_INCR,
+     "fricking": B_INCR, "frickin": B_INCR, "frigging": B_INCR, "friggin": B_INCR, "fully": B_INCR, 
+     "fuckin": B_INCR, "fucking": B_INCR, "fuggin": B_INCR, "fugging": B_INCR,
+     "greatly": B_INCR, "hella": B_INCR, "highly": B_INCR, "hugely": B_INCR, 
+     "incredible": B_INCR, "incredibly": B_INCR, "intensely": B_INCR, 
+     "major": B_INCR, "majorly": B_INCR, "more": B_INCR, "most": B_INCR, "particularly": B_INCR,
      "purely": B_INCR, "quite": B_INCR, "really": B_INCR, "remarkably": B_INCR,
      "so": B_INCR, "substantially": B_INCR,
-     "thoroughly": B_INCR, "totally": B_INCR, "tremendously": B_INCR,
-     "uber": B_INCR, "unbelievably": B_INCR, "unusually": B_INCR, "utterly": B_INCR,
+     "thoroughly": B_INCR, "total": B_INCR, "totally": B_INCR, "tremendous": B_INCR, "tremendously": B_INCR,
+     "uber": B_INCR, "unbelievably": B_INCR, "unusually": B_INCR, "utter": B_INCR, "utterly": B_INCR,
      "very": B_INCR,
      "almost": B_DECR, "barely": B_DECR, "hardly": B_DECR, "just enough": B_DECR,
      "kind of": B_DECR, "kinda": B_DECR, "kindof": B_DECR, "kind-of": B_DECR,
-     "less": B_DECR, "little": B_DECR, "marginally": B_DECR, "occasionally": B_DECR, "partly": B_DECR,
-     "scarcely": B_DECR, "slightly": B_DECR, "somewhat": B_DECR,
+     "less": B_DECR, "little": B_DECR, "marginal": B_DECR, "marginally": B_DECR,
+     "occasional": B_DECR, "occasionally": B_DECR, "partly": B_DECR,
+     "scarce": B_DECR, "scarcely": B_DECR, "slight": B_DECR, "slightly": B_DECR, "somewhat": B_DECR,
      "sort of": B_DECR, "sorta": B_DECR, "sortof": B_DECR, "sort-of": B_DECR}
 
 # check for sentiment laden idioms that do not contain lexicon words (future work, not yet implemented)
